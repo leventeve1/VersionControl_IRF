d74081720643f4d9d7fcbc815ae4691f87c5226c
leventeve1 <selleylevente@gmail.com>
2022-11-03 18:17:25 +0100

    labda szinezese

--- hatodik_beadando/hatodik_beadando/Form1.Designer.cs
+++ hatodik_beadando/hatodik_beadando/Form1.Designer.cs
@@ -32,6 +32,10 @@
             this.mainPanel = new System.Windows.Forms.Panel();
             this.createTimer = new System.Windows.Forms.Timer(this.components);
             this.conveyorTimer = new System.Windows.Forms.Timer(this.components);
+            this.button_car = new System.Windows.Forms.Button();
+            this.button_ball = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button_color = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // mainPanel
@@ -39,9 +43,9 @@
             this.mainPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.mainPanel.Location = new System.Drawing.Point(1, 0);
+            this.mainPanel.Location = new System.Drawing.Point(1, 330);
             this.mainPanel.Name = "mainPanel";
-            this.mainPanel.Size = new System.Drawing.Size(801, 452);
+            this.mainPanel.Size = new System.Drawing.Size(801, 122);
             this.mainPanel.TabIndex = 0;
             // 
             // createTimer
@@ -56,15 +60,59 @@
             this.conveyorTimer.Interval = 10;
             this.conveyorTimer.Tick += new System.EventHandler(this.conveyorTimer_Tick);
             // 
+            // button_car
+            // 
+            this.button_car.Location = new System.Drawing.Point(13, 13);
+            this.button_car.Name = "button_car";
+            this.button_car.Size = new System.Drawing.Size(97, 84);
+            this.button_car.TabIndex = 1;
+            this.button_car.Text = "CAR";
+            this.button_car.UseVisualStyleBackColor = true;
+            this.button_car.Click += new System.EventHandler(this.button_car_Click);
+            // 
+            // button_ball
+            // 
+            this.button_ball.Location = new System.Drawing.Point(117, 13);
+            this.button_ball.Name = "button_ball";
+            this.button_ball.Size = new System.Drawing.Size(101, 84);
+            this.button_ball.TabIndex = 2;
+            this.button_ball.Text = "BALL";
+            this.button_ball.UseVisualStyleBackColor = true;
+            this.button_ball.Click += new System.EventHandler(this.button_ball_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(385, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Coming next:";
+            // 
+            // button_color
+            // 
+            this.button_color.BackColor = System.Drawing.Color.Gold;
+            this.button_color.Location = new System.Drawing.Point(117, 113);
+            this.button_color.Name = "button_color";
+            this.button_color.Size = new System.Drawing.Size(101, 30);
+            this.button_color.TabIndex = 4;
+            this.button_color.UseVisualStyleBackColor = false;
+            this.button_color.Click += new System.EventHandler(this.button_color_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button_color);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button_ball);
+            this.Controls.Add(this.button_car);
             this.Controls.Add(this.mainPanel);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -73,6 +121,10 @@
         private System.Windows.Forms.Panel mainPanel;
         private System.Windows.Forms.Timer createTimer;
         private System.Windows.Forms.Timer conveyorTimer;
+        private System.Windows.Forms.Button button_car;
+        private System.Windows.Forms.Button button_ball;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button_color;
     }
 }
 
--- hatodik_beadando/hatodik_beadando/Form1.cs
+++ hatodik_beadando/hatodik_beadando/Form1.cs
@@ -15,11 +15,18 @@ namespace hatodik_beadando
     public partial class Form1 : Form
     {
         private List<Toy> _toys = new List<Toy>();
+
+        private Toy _nextToy;
+
         private IToyFactory _factory;
         public IToyFactory Factory
         {
             get { return _factory;}
-            set { _factory = value;}
+            set
+            {
+                _factory = value;
+                DisplayNext();
+            }
         }
         public Form1()
         {
@@ -51,6 +58,40 @@ namespace hatodik_beadando
                 mainPanel.Controls.Remove(oldestBall);
                 _toys.Remove(oldestBall);
             }
+        }
+
+        private void DisplayNext()
+        {
+            if (_nextToy != null)
+                Controls.Remove(_nextToy);
+            _nextToy = Factory.CreateNew();
+            _nextToy.Top = label1.Top + label1.Height + 20;
+            _nextToy.Left = label1.Left;
+            Controls.Add(_nextToy);
+        }
+
+        private void button_car_Click(object sender, EventArgs e)
+        {
+            Factory = new CarFactory();
+        }
+
+        private void button_ball_Click(object sender, EventArgs e)
+        {
+            Factory = new BallFactory
+            {
+                BallColor = button_color.BackColor
+            };
+        }
+
+        private void button_color_Click(object sender, EventArgs e)
+        {
+            var button = (Button)sender;
+            var colorPicker = new ColorDialog();
+
+            colorPicker.Color = button.BackColor;
+            if (colorPicker.ShowDialog() != DialogResult.OK)
+                return;
+            button.BackColor = colorPicker.Color;
         }
     }
 }
