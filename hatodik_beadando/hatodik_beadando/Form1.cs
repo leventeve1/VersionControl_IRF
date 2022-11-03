5c04f13727e8f9407d750dff9e6d89d0b782e75f
leventeve1 <selleylevente@gmail.com>
2022-11-03 17:46:35 +0100

    Toy és Car osztályok

--- hatodik_beadando/hatodik_beadando/Form1.cs
+++ hatodik_beadando/hatodik_beadando/Form1.cs
@@ -39,7 +39,7 @@ namespace hatodik_beadando
             var maxPosition = 0;
             foreach (var ball in _balls)
             {
-                ball.MoveBall();
+                ball.MoveToy();
                 if (ball.Left > maxPosition)
                     maxPosition = ball.Left;
             }
