9ea72ce39938ca511647994b6654cf9d518152b9
leventeve1 <selleylevente@gmail.com>
2022-11-10 16:12:49 +0100

    bonyolult számítások bemásolása

--- hetedik_beadando_VaR/hetedik_beadando_VaR/Form1.Designer.cs
+++ hetedik_beadando_VaR/hetedik_beadando_VaR/Form1.Designer.cs
@@ -41,17 +41,17 @@
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(361, 315);
+            this.dataGridView1.Size = new System.Drawing.Size(377, 386);
             this.dataGridView1.TabIndex = 0;
             // 
             // dataGridView2
             // 
             this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView2.Location = new System.Drawing.Point(380, 13);
+            this.dataGridView2.Location = new System.Drawing.Point(396, 13);
             this.dataGridView2.Name = "dataGridView2";
             this.dataGridView2.RowHeadersWidth = 51;
             this.dataGridView2.RowTemplate.Height = 24;
-            this.dataGridView2.Size = new System.Drawing.Size(321, 315);
+            this.dataGridView2.Size = new System.Drawing.Size(376, 386);
             this.dataGridView2.TabIndex = 1;
             // 
             // Form1
