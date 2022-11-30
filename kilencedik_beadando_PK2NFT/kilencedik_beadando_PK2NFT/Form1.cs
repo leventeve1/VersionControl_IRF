d589c8a6a92d1dc8e16874220ec32177e5e4cf4f
leventeve1 <selleylevente@gmail.com>
2022-11-30 18:05:38 +0100

    projekt letrehozas

---
+++ kilencedik_beadando_PK2NFT/kilencedik_beadando_PK2NFT/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kilencedik_beadando_PK2NFT
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
